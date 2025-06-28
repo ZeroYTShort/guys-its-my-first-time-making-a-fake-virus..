cff9ba47a98845607749dc02a18f5a8bc8a5ec95
ZeroYTShort <terwtragamer29@gmail.com>
2025-06-28 04:36:41 -1000

    Update anti.virus.bat

--- anti.virus.bat
+++ anti.virus.bat
@@ -1,1 +1,85 @@
+@echo off
+color 2
+echo This test virus it has sometimes a bug so if this happends reopen the app!!!!
+echo Creator: @ZeroYTShorts
+echo YT Link: www.youtube.com/@ZeroYTShorts
 
+timeout 5
+goto start
+
+
+:start
+@echo off
+timeout 1
+color 57
+
+echo hey,do you want anti-virus app? :3 (only answer in yes or no)
+
+set/p love=
+
+if %love%==yes goto love
+if %love%==no goto hate
+
+:love
+@echo oon
+echo yoooo now u have anti virus:3 np
+
+pause
+exit
+
+:hate
+@echo off
+echo bro:( 
+
+echo choose:
+echo what
+echo lol
+
+set/p ggr=
+if %ggr%==what goto macaca
+if %ggr%==lol goto cute
+
+:cute
+@echo off
+color 3
+echo why are you a bad guy?:(
+timeout 2
+
+@echo off
+color 3
+echo you hate me?:(
+
+echo write:
+echo yea
+echo nope
+
+set/p nhh=
+if %nhh%==nope goto bad
+if %nhh%==yea goto happy
+
+:happy
+@echo off
+color 4
+echo OH YEAH? ALRIGHT IMA HACK YOUR PC>:)in 5 sec
+goto loop 2
+stop
+
+:bad
+@echo off
+color 3
+echo alright see ya
+timeout 5
+exit
+stop
+
+:macaca
+@echo off
+echo see ya lil guy >:(
+timeout 3
+stop
+
+@echo off
+color 4   
+:loop
+echo WHY WHY WHY WHY WHY WHY WHY WHY WHY WHY WHY WHY WHY                                                                                       
+goto loop
